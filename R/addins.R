43382f426a187cbfe4d42d5ba64c8db3306278f0
phgrosjean <phgrosjean@sciviews.org>
2020-08-18 13:00:23 +0200

    RStudio addin for webshot_shiny() + modified Travis CI specif to compile the magick package

---
+++ R/addins.R
@@ -0,0 +1,45 @@
+# RStudio addins
+
+
+# Shiny Application Webshot -----------------------------------------------
+
+webshot_shiny_addin <- function() {
+  # Gets The active Document
+  ctx <- rstudioapi::getActiveDocumentContext()
+
+  # Checks that a document is active
+  if (!is.null(ctx)) {
+
+    # Extracts selection as a string
+    sel <- ctx$selection[[1]]$text
+
+    # Check that the selection contains someting like a call to a function
+    if (!grepl("^[^\\(]+(\\([^\\)]+\\)).*$", sel)) {
+      message("You must select a construct like 'launch_shiny(url, ...)' first")
+      return()
+    }
+
+    # Get what looks like the arguments
+    args <- sub("^[^\\(]+(\\([^\\)]+\\)).*$", "\\1", sel)
+
+    # Construct the code to take screenshot of the Shiny app and run it
+    message("Launching the Shiny application and taking the screenshot, please wait...")
+    img <- NULL
+    code <- paste0("img <- learndown::webshot_shiny", args)
+    res <- try(eval(parse(text = code)), silent = TRUE)
+
+    # Is there an error?
+    if (inherits(res, "try-error")) {
+      message("An error occured while trying to take Shiny app screenshot")
+      message(res)
+    } else if (is.null(img)) {
+      # No screenshot was created
+      message("The screenshot was not created, check your code!")
+    } else {
+      # Check result and display the image and a message to the R console
+      message("Screenshot created in: ", img)
+      print(magick::image_read(img))
+    }
+  }
+}
+
--- TODO.md
+++ TODO.md
@@ -2,4 +2,10 @@
 
 - Tabulations in bookdown
 
+- `include_shiny()` for direct inclusion of Shiny applications, like knitr::include_app(), but with more options (like width and frameborder). This version would start the Shiny app directly in the page (simple iframe)
+
+- `run_shiny()` shows the same screenshot as `launch_shiny()` but does not offer the possibility to run it on the page. The user has to run it in its SciViews Box, but it would start an URL doing so if the svbox is already running
+
+- Content display with an expiration date (e.g., for Github Classroom assigments) with alternate text
+
 - Automatic light and dark theme
